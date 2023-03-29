c01729385a464ce6f1f3ebaab8a31d8f27fef7ea
edenmorr <93930203+edenmorr@users.noreply.github.com>
2023-03-29 15:06:59 +0300

    not finnished

---
+++ Player.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+namespace ariel{
+    class Player{
+        string name;
+        Player(string name){
+            
+        }
+    }
+}
