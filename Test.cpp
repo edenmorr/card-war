ade6c34e360cbe8e457509cc7704176393940b04
edenmorr <93930203+edenmorr@users.noreply.github.com>
2023-03-30 19:46:51 +0300

    h

--- Test.cpp
+++ Test.cpp
@@ -33,14 +33,46 @@
     Game game(p1,p2); 
     CHECK_EQ(p1.stacksize(), p2.stacksize());
     }
-TEST_CASE("Test number 4: ") { //cheacking the amount of card the winning player have and the amonut the other player have 
+    TEST_CASE("Test number 4: ") { //cheacking the amount of card the winning player have and the amonut the other player have 
     Player p1("Alice");
     Player p2("Bob");
     Game game(p1, p2);
     CHECK(p1.cardesTaken() == 52 );
     CHECK(p2.stacksize() == 0);
 }
-   
+ 
+TEST_CASE("Test number 5: ") {//"cheack players' cards after 10 turns"
+    Player p1("Alice");
+    Player p2("Bob");
+
+    Game game(p1, p2);
+    for (int i = 0; i < 10; i++) {
+        game.playTurn();
+    }
+    CHECK((p1.stacksize() == 16) || (p2.stacksize() == 16)); 
+    CHECK((p1.stacksize() == 36) || (p2.stacksize() == 36)); 
+}
+
+   TEST_CASE("Test number 6: ") {//"cheack stacksize after 2 turns 
+    Player p1("Alice");
+    Player p2("Bob");
+    Game game(p1, p2);
+    int stacksize1 = game.player1.stacksize();
+    int stacksize2 = game.player2.stacksize();
+        game.playTurn();
+    CHECK(game.player1.stacksize() == (stacksize1 - 1) || game.player2.stacksize() == (stacksize2 - 1) );
+        game.playTurn();
+    CHECK(game.player1.stacksize() == (stacksize1 - 1) || game.player2.stacksize() == (stacksize2 - 1) );
+   }
 
 
-
+   TEST_CASE("Test number 6: ") {//"cheack stacksize after 2 turns 
+    Player p1("Alice");
+    Player p2("Bob");
+    Game game(p1, p2);
+    for (int i = 0; i < 5; i++) {
+        game.playTurn();
+    }
+    CHECK(game.player1.stacksize() < 21 || game.player1.stacksize() == 21 );
+    CHECK(game.player2.stacksize() < 21 || game.player2.stacksize() == 21 );
+   }
